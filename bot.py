ab754f2cefa1547682a9562442321aa36fcb8a93
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2024-11-04 16:11:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,3 @@
-
-
 import logging
 from telegram.ext import (
     filters,
@@ -7,25 +5,36 @@ from telegram.ext import (
     CommandHandler,
     MessageHandler
 )
-
 from plugins.__main__ import *
 from config import Config
+from flask import Flask, request
+
+app = Flask(__name__)
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
+# Flask route for webhook
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    json_update = request.get_json()
+    bot.update_queue.put(json_update)
+    return "OK"
 
 def main():
     bot = ApplicationBuilder().token(Config.BOT_TOKEN).build()
     bot.add_handler(CommandHandler('start', startMessage))
     bot.add_handler(CommandHandler('help', helpMessage))
     bot.add_handler(MessageHandler(filters.REPLY, handleCaption))
-    bot.add_handler(MessageHandler(
-        filters.ALL & ~filters.COMMAND, handleMessage))
-    bot.run_polling()
+    bot.add_handler(MessageHandler(filters.ALL & ~filters.COMMAND, handleMessage))
 
+    # Set the webhook URL
+    bot.bot.set_webhook(url=Config.WEBHOOK_URL)
+
+    # Start Flask server
+    app.run(host='0.0.0.0', port=5000)
 
 if __name__ == '__main__':
     main()
